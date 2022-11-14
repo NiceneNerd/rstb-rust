ec75a749ca72a374c2964867eb15c8497941240b
GingerAvalanche <chodness@gmail.com>
2022-11-13 20:36:50 -0800

    bxml.rs: Isolate overheads by console
    Also brings the code into line with conventions used with the other parse size functions

--- src/calc/cpp_memsizes/bxml.rs
+++ src/calc/cpp_memsizes/bxml.rs
@@ -5,16 +5,16 @@ use crate::Endian;
 const CLASS_SIZE_WIIU: usize = 0x4a8;
 const CLASS_SIZE_NX: usize = 0x778;
 
-const BXML_OVERHEAD: usize = 0x64;
+const OVERHEAD_WIIU: usize = 0x64;
+const OVERHEAD_NX: usize = 0x44;
 const TAG_SIZE: usize = std::mem::size_of::<u32>();
 
-pub fn parse_size(b: &[u8], endian: Endian) -> u32 {
+pub fn parse_size(bytes: &[u8], endian: Endian) -> u32 {
     let mut total_size: usize = match endian {
-        Endian::Big => super::PARSE_CONST_WIIU as usize + CLASS_SIZE_WIIU,
-        Endian::Little => super::PARSE_CONST_NX as usize + CLASS_SIZE_NX,
+        Endian::Big => super::PARSE_CONST_WIIU as usize + CLASS_SIZE_WIIU + OVERHEAD_WIIU,
+        Endian::Little => super::PARSE_CONST_NX as usize + CLASS_SIZE_NX + OVERHEAD_NX,
     };
-    total_size += BXML_OVERHEAD;
-    let a: ParameterIO = ParameterIO::from_binary(b).unwrap();
+    let a = ParameterIO::from_binary(bytes).unwrap();
 
     if let Some(tags) = a.param_root.objects.get("Tags") {
         total_size += TAG_SIZE * tags.len();
